14c31c5dda4fc85d7b033283a2162ecd9f3a4f9b
shahriarradnoei <131372291+shahriarradnoei@users.noreply.github.com>
2023-11-26 14:49:55 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function button1(){
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+}
+function button2(){
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+}
+function button3(){
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+}
+function button4(){
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+}
+function button5(){
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+}
+function button6(){
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+}
+function button7(){
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+}
